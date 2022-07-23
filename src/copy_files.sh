211cbbf4cb7d6420f514b9c19be6a0948a67bd34
Natalie <nwagenbrenner@gmail.com>
2022-07-23 10:46:31 -0600

    update cp script for machine location changes

--- src/copy_files.sh
+++ src/copy_files.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
 #copy the WRF kml files
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/wrf*.kml natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/wrf*.kml ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/wrf
 
 #copy the WRF legend files
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.bmp natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.bmp ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/wrf_key
 
 #copy the WRF legend files
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/wxLog.txt natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/wxLog.txt ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim
 
 #copy the WindNinja output files for download from the hires-wrf webpage
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.kmz natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf/ninjaout
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.shp natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf/ninjaout
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.shx natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf/ninjaout
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.dbf natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf/ninjaout
-scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.prj natalie@fwas.usfs-i2.umt.edu:/home/natalie/nightly_wrf/ninjaout
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.kmz ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.shp ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.shx ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.dbf ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.prj ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/ninjaout/*.asc ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
+
+#copy the wrfout file
+scp /media/natalie/ExtraDrive2/nightly_wrf/output/out.nc ubuntu@192.168.59.7:/home/ubuntu/ninjaInput/wrfSim/ninjaout/1km_wrf
