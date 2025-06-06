fa99356838126eee34a5ee7cfce1b9f57a91ee10
Natalie Wagenbrenner <nwagenbrenner@gmail.com>
2025-06-06 12:36:59 -0600

    update print for python3

--- src/fetchHRRR.py
+++ src/fetchHRRR.py
@@ -17,7 +17,7 @@ timeList = ['12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '2
     #dataFile = 'http://nomads.ncep.noaa.gov/pub/data/nccf/com/hrrr/prod/hrrr.%s/conus/hrrr.t12z.wrfprsf%s.grib2'\
 
 for time in timeList:
-    print 'Downloading t06z.wrfprsf%s.grib2' % time
+    print('Downloading t06z.wrfprsf%s.grib2' % time)
     dataFile = 'http://nomads.ncep.noaa.gov/pub/data/nccf/com/hrrr/prod/hrrr.%s/conus/hrrr.t06z.wrfprsf%s.grib2'\
                 % (date, time)
     p = subprocess.Popen(["wget %s" % dataFile], cwd = dataDir, shell = True, stdout=subprocess.PIPE)
