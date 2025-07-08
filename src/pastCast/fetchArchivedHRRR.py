307992db0d73d951ac86edd7947b0c2f9b054e04
Natalie Wagenbrenner <nwagenbrenner@gmail.com>
2025-07-08 12:36:36 -0600

    add fetchArchivedHRRR that reads input datetimes from a config.json

---
+++ src/pastCast/fetchArchivedHRRR.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import subprocess
+import datetime
+import json
+import sys
+
+def usage():
+    print("Not enough args!!!\n")
+    print("Usage:\n")
+    print("fetchHRRR path/to/config.json\n")
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit()
+
+config_path = sys.argv[1]
+print("The input config_path is: %s" % config_path)
+
+#=============================================================================
+#        Setup for current fetch
+#=============================================================================
+with open(config_path, 'r') as f:
+    config = json.load(f)
+
+dataDir = config['paths']['dataDir']
+startYear = config['datetime']['startYear']
+startMonth = config['datetime']['startMonth']
+startDay = config['datetime']['startDay']
+startHour = config['datetime']['startHour']
+startMinute = config['datetime']['startMinute']
+startSecond = config['datetime']['startSecond']
+endYear = config['datetime']['endYear']
+endMonth = config['datetime']['endMonth']
+endDay = config['datetime']['endDay']
+endHour = config['datetime']['endHour']
+endMinute = config['datetime']['endMinute']
+endSecond = config['datetime']['endSecond']
+
+print("start datetime: %s-%s-%s-%s:%s:%s" % (startYear, startMonth, startDay, startHour, startMinute, startSecond))
+print("end datetime: %s-%s-%s-%s:%s:%s" % (endYear, endMonth, endDay, endHour, endMinute, endSecond))
+
+startStr = startYear+startMonth+startDay+startHour
+endStr = endYear+endMonth+endDay+endHour
+
+print("startStr = %s" % startStr)
+print("endStr = %s" % endStr)
+
+# Convert to datetime objects
+startDt = datetime.datetime.strptime(startStr, '%Y%m%d%H')
+endDt = datetime.datetime.strptime(endStr, '%Y%m%d%H')
+
+# Generate list of datetimes
+datetimeList = []
+currentDt = startDt
+while currentDt <= endDt:
+    datetimeList.append(currentDt.strftime('%Y%m%d%H'))
+    currentDt += datetime.timedelta(hours=1)
+
+# Print result
+print(datetimeList)
+
+for datetime in datetimeList:
+    dataFile = '\"gs://high-resolution-rapid-refresh/hrrr.%s/conus/hrrr.t%sz.wrfprsf00.grib2\"'\
+            % (datetime[:8], datetime[-2:])
+    print('Downloading %s' % dataFile)
+    p = subprocess.Popen(["gsutil -m cp %s ." % dataFile], cwd = dataDir, shell = True, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+
+print("Download complete!")
+
+
