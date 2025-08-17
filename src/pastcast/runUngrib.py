ece7afe67d3e0cf0c3ed66ca4fc3b10b1ff1f828
Natalie Wagenbrenner <nwagenbrenner@gmail.com>
2025-08-17 14:14:40 -0600

    update path in config and change runUngrib to accept a config file

--- src/pastcast/runUngrib.py
+++ src/pastcast/runUngrib.py
@@ -2,9 +2,29 @@
 
 import subprocess
 import datetime
+import json
+import sys
 
-WPS = '/home/natalie/src/wrf/WPS/'
-dataDir = '/home/natalie/hires_wrf/data/'
+def usage():
+    print("Not enough args!!!\n")
+    print("Usage:\n")
+    print("runUngrib path/to/config.json\n")
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit()
+
+config_path = sys.argv[1]
+print("The input config_path is: %s" % config_path)
+
+#=============================================================================
+#        Setup paths for current domain
+#=============================================================================
+with open(config_path, 'r') as f:
+    config = json.load(f)
+
+WPS = config['paths']['wpsDir']
+dataDir = config['paths']['dataDir']
 
 print("WPS=%s" % WPS)
 print("dataDir=%s" % dataDir)
@@ -22,6 +42,7 @@ out, err = p.communicate()
 #=============================================================================
 #        Run ungrib.exe
 #=============================================================================
-p = subprocess.Popen(["./ungrib.exe"], cwd = WPS, shell = True, stdout=subprocess.PIPE)
+#p = subprocess.Popen(["./ungrib.exe", ">&", "log.ungrib"], cwd = WPS, shell = True, stdout=subprocess.PIPE)
+p = subprocess.Popen(["export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/jasper-1.900.29/lib/ && ./ungrib.exe"], cwd = WPS, shell = True, stdout=subprocess.PIPE)
 out, err = p.communicate()
 
